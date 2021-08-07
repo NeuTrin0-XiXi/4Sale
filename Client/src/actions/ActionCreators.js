233549195277cca492ea6a2076cc941b895ffec2
Jaisurya <jaisuryak11@gmail.com>
2021-08-07 12:08:02 +0530

    React-redux setup

--- Client/src/actions/ActionCreators.js
+++ Client/src/actions/ActionCreators.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const getItems = () => dispatch => {
     dispatch(setItemsLoading());
 
-    axios.get('/4Sale')             //dOUBT ---> what is this '/4Sale' here  exactly  ?
+    axios.get('/items')            
     .then(res => dispatch({
         type:GET_ITEMS,
         payload: res.data
@@ -14,7 +14,7 @@ export const getItems = () => dispatch => {
 
 
 export const addItem = (item) => dispatch => {
-    axios.post('/sell' , item)
+    axios.post('/items' , item)
     .then(res => dispatch({
         type: ADD_ITEMS,
         payload: res.data
--- routes/item.js
+++ routes/item.js
@@ -1,49 +1,60 @@
 //Route module for handeling queries regarding items
 
-const express=require('express');
-const route=express.Router();
-const Item=require('../db/models').itemModel;
-const User=require('../db/models').userModel;
+const express = require('express');
+const route = express.Router();
+const Item = require('../db/models').itemModel;
+const User = require('../db/models').userModel;
 
 
 //API handlers
 
 //GET Handlers:      
 //GET all items:
-route.get('/',(req,res,next)=>{
-    Item.find({}).then((item)=>{
-        res.status(200).send(item)
-    }).catch(next);
+route.get('/', (req, res, next) => {
+    Item.find({})
+        .select('name date price')
+        .then((item) => {
+            res.status(200).send(item)
+        })
+        .catch(next);
 });
 
 
 //GET Items from Search Bar(name):                  
-route.get('/search',(req,res,next)=>{
-    Item.find(res.query).then((item)=>{
-        res.status(200).send(item);
-    }).catch(next);
+route.get('/search', (req, res, next) => {
+    Item.find(res.query)
+        .select('name date price')
+        .then((item) => {
+            res.status(200).send(item);
+        })
+        .catch(next);
 
 });
 
 
 //GET Items from Filters:                           
-route.get('/filter',(req,res,next)=>{
-    if(req.query.priceUb!=undefined && req.query.priceLb!=undefined){
-        req.query.price={$lte: req.query.priceUb,$gte: req.query.priceLb};
+route.get('/filter', (req, res, next) => {
+    if (req.query.priceUb != undefined && req.query.priceLb != undefined) {
+        req.query.price = { $lte: req.query.priceUb, $gte: req.query.priceLb };
         delete req.query.priceUb;
         delete req.query.priceLb;
     }
-    Item.find(req.query).then((item)=>{
-        res.status(200).send(item);
-    }).catch(next);
+    Item.find(req.query)
+        .select('name date price')
+        .then((item) => {
+            res.status(200).send(item);
+        })
+        .catch(next);
 });
 
 
 //GET Item details:                             
-route.get('/:id',(req,res,next)=>{
-    Item.findById(req.params.id).then((item)=>{
-        res.status(200).send(item)
-    }).catch(next);
+route.get('/:id', (req, res, next) => {
+    Item.findById(req.params.id)
+        .then((item) => {
+            res.status(200).send(item)
+        })
+        .catch(next);
 });
 
 
@@ -52,34 +63,42 @@ route.get('/:id',(req,res,next)=>{
 
 //post an item
 
-route.post('/',(req,res,next)=>{
-    Item.create(req.body).then((item)=>{
-        res.status(200).send(`Posted ${item.name} sucessfully`);
-    }).catch(next);
+route.post('/', (req, res, next) => {
+    Item.create(req.body)
+        .then((item) => {
+            res.status(201).send(`Posted ${item.name} sucessfully`);
+        })
+        .catch(next);
 });
 
 //--------------------------------------------------------------------------//
 
 //Edit a posted item                               
-route.put('/:id',(req,res,next)=>{
-    Item.updateOne({_id: req.params.id},req.body).then((item)=>{
-    }).then(
-        Item.findById(req.params.id).then((item)=>{
-            res.status(200).send(`${item.name} has been updated`);
+route.put('/:id', (req, res, next) => {
+    Item.updateOne({ _id: req.params.id }, req.body)
+        .then((item) => {
         })
-    ).catch(next);
+        .then(
+            Item.findById(req.params.id)
+                .then((item) => {
+                    res.status(200).send(`${item.name} has been updated`);
+                })
+        )
+        .catch(next);
 });
 
 //-------------------------------------------------------------------------//
 
 
 //Delete a posted item                              //Get back item name
-route.delete('/:id',(req,res,next)=>{
-    Item.deleteOne({_id: req.params.id}).then((item)=>{
-        res.status(200).send(`${item.name} has been removed`);
-    }).catch(next);
+route.delete('/:id', (req, res, next) => {
+    Item.deleteOne({ _id: req.params.id })
+        .then((item) => {
+            res.status(200).send(`${item.name} has been removed`);
+        })
+        .catch(next);
 });
 
 
 
-module.exports=route;
+module.exports = route;
