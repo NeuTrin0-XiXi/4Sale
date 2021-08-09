d63bfda4b0850bdf474331b5ee8018abdc3636f9
Jaisurya <jaisuryak11@gmail.com>
2021-08-09 09:38:39 +0530

    fetch

--- Server/server.js
+++ Server/server.js
@@ -11,17 +11,6 @@ app.use(express.static('public'));
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
-<<<<<<< HEAD
-mongoose.connect('mongodb://localhost:27017/4Sale1')
-.then(() => {console.log("MongoDB connected successfully ....")})
-.catch((err) => {console.log("MongoDB connection unsuccessfull ....")});
-
-app.use('/' ,items);
-
-app.use((err,req,res,next)=>{
-    res.send(err.message);
-})
-=======
 mongoose.connect('mongodb://localhost:27017/4Sale')
 .then(() => {console.log("MongoDB connected successfully ....")})
 .catch((err) => {console.log("MongoDB connection unsuccessfull ....")});
@@ -29,8 +18,6 @@ mongoose.connect('mongodb://localhost:27017/4Sale')
 mongoose.Promise = global.Promise;
 
 app.use('/4Sale' , items);
->>>>>>> 468942398371a1a8811ef405923f83b50a07bc3c
-
 const port = 5000;
 
 app.listen(port , console.log(`Server running at port ${port}`));
