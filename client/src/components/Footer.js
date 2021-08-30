46faf0fbb2879d73700e58c59dba69d2b4433332
Arush Pradhan <85868958+13suk@users.noreply.github.com>
2021-08-30 17:09:44 +0530

    Footer Commits

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -27,15 +27,25 @@ const Footer = () => {
                         </div>
                         <div className="col-12 col-md-3" style={column}>
                             <Link to="/about-us" style={textColor}><h3>About Us</h3></Link>
-                            <span>text text text</span>
-                            <span>text text text</span>
-                            <span>text text text</span>
+                             <div>
+                            <span><strong><u>Our Team</u></strong></span>
+                            <br />
+                            <span>Jaisurya Katla</span>
+                            <br />
+                            <span>Suman Jaiswal</span>
+                            <br />
+                            <span>Kanishka Goyal</span>
+                            <br />
+                            <span>Arush Pradhan</span>
+                            </div>
                         </div>
                         <div className="col-12 col-md-3" style={column}>
                             <Link to="/contact-us" style={textColor}><h3>Contact Us</h3> </Link>
-                            <span>text text text</span>
-                            <span>text text text</span>
-                            <span>text text text</span>
+                            <div style = {{position: 'relative' , left: '30px'}}>
+                            <span><strong><u>Phone no</u> :918XXXXXXX</strong></span>
+                            <br/>
+                            <span><strong><u>e-mail Id</u>: 4Sale@iiti.ac.in</strong></span>
+                        </div>
                         </div>
                         <div className="col-12 col-md-3" style={column}>
                             <h3 ><a href="https://www.google.com/maps/place/IIT+Indore/@22.5211819,75.9186987,17.97z/data=!4m5!3m4!1s0x3962efcccbce7145:0x784e8cb69818596b!8m2!3d22.5203597!4d75.9207231?hl=en" style={textColor}>Visit Us</a></h3>
