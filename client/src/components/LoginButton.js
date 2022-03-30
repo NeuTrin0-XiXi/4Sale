3df42440556ff42064a9100c784ec984aae8f96c
Jaisurya <jaisuryak11@gmail.com>
2022-03-30 16:56:10 +0530

    test 1

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -41,7 +41,7 @@ function LoginButton(props) {
         <div className='m-auto' >
             <GoogleLogin
                 id="navLoginButton"
-                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                clientId={process.env.GOOGLE_CLIENT_ID}
                 // clientId={clientID}
                 buttonText="Login"
                 hostedDomain={"iiti.ac.in"}
