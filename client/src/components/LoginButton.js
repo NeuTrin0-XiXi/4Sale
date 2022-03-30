ca5b523cfc96de31f2a5f71db797fd0de7523be8
Jaisurya <jaisuryak11@gmail.com>
2022-03-30 17:01:12 +0530

    test 1.2

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import GoogleLogin from 'react-google-login';
 import axios from 'axios';
-import clientID from '../googleClient';
+// import clientID from '../googleClient';
 
 function LoginButton(props) {
 
